ab83e5a8f4a5b3af10b8c55839467193974b39be
Ibrahim M <145262661+Ibi-M@users.noreply.github.com>
2024-11-08 10:23:52 +0000

    Changes Made

--- memory_match.py
+++ memory_match.py
@@ -14,31 +14,41 @@ class color:
 
 def progress():
     column_width = 10
-    print (color.BOLD + color.UNDERLINE + " ".center(column_width) + "|" +"1".center(column_width) + "|" + "2".center(column_width) + "|" + "3".center(column_width) + "|" + "4".center(column_width) + "|" +"5".center(column_width) + color.END)
-    print (color.BOLD +"1".center(column_width) + color.END + "|" + player[0][0].center(column_width) + "|" + player[0][1].center(column_width) + "|" + player[0][2].center(column_width) + "|" + player[0][3].center(column_width) + "|" + player[0][4].center(column_width))
-    print (color.BOLD +"2".center(column_width) + color.END + "|" + player[1][0].center(column_width) + "|" + player[1][1].center(column_width) + "|" + player[1][2].center(column_width) + "|" + player[1][3].center(column_width) + "|" + player[1][4].center(column_width))
-    print (color.BOLD +"3".center(column_width) + color.END + "|" + player[2][0].center(column_width) + "|" + player[2][1].center(column_width) + "|" + player[2][2].center(column_width) + "|" + player[2][3].center(column_width) + "|" + player[2][4].center(column_width))
-    print (color.BOLD +"4".center(column_width) + color.END + "|" + player[3][0].center(column_width) + "|" + player[3][1].center(column_width) + "|" + player[3][2].center(column_width) + "|" + player[3][3].center(column_width) + "|" + player[3][4].center(column_width))
-    print (color.BOLD +"5".center(column_width) + color.END + "|" + player[4][0].center(column_width) + "|" + player[4][1].center(column_width) + "|" + player[4][2].center(column_width) + "|" + player[4][3].center(column_width) + "|" + player[4][4].center(column_width))
-
+    print ("____________________________________________________________________")
+    print(color.BOLD + color.UNDERLINE + "|" + " ".center(column_width) + "|" + color.CYAN + "1".center(column_width) + "|" + "2".center(column_width) + "|" + "3".center(column_width) + "|" + "4".center(column_width) + "|" + "5".center(column_width) + color.END)
+    print(color.BOLD + color.YELLOW + "|" + "1".center(column_width) + "|" + color.END + player[0][0].center(column_width) + "|" + player[0][1].center(column_width) + "|" + player[0][2].center(column_width) + "|" + player[0][3].center(column_width) + "|" + player[0][4].center(column_width) + "|")
+    print(color.BOLD + color.YELLOW + "|" + "2".center(column_width) + "|" + color.END + player[1][0].center(column_width) + "|" + player[1][1].center(column_width) + "|" + player[1][2].center(column_width) + "|" + player[1][3].center(column_width) + "|" + player[1][4].center(column_width) + "|")
+    print(color.BOLD + color.YELLOW + "|" + "3".center(column_width) + "|" + color.END + player[2][0].center(column_width) + "|" + player[2][1].center(column_width) + "|" + player[2][2].center(column_width) + "|" + player[2][3].center(column_width) + "|" + player[2][4].center(column_width) + "|")
+    print(color.BOLD + color.YELLOW + "|" + "4".center(column_width) + "|" + color.END + player[3][0].center(column_width) + "|" + player[3][1].center(column_width) + "|" + player[3][2].center(column_width) + "|" + player[3][3].center(column_width) + "|" + player[3][4].center(column_width) + "|")
+    print(color.BOLD + color.YELLOW + "|" + "5".center(column_width) + "|" + color.END + player[4][0].center(column_width) + "|" + player[4][1].center(column_width) + "|" + player[4][2].center(column_width) + "|" + player[4][3].center(column_width) + "|" + player[4][4].center(column_width) + "|")
+    print ("-----------------------------------------------------------------")
+def get_valid_input(prompt_row, prompt_col):
+    while True:
+        try:
+            row = int(input(prompt_row)) - 1
+            col = int(input(prompt_col)) - 1
+            if row < 0 or row >= 5 or col < 0 or col >= 5:
+                print ("")
+                print(color.RED + "Invalid position. Please choose a row and column between 1 and 5." + color.END)
+                print ("")
+                continue
+            if player[row][col] != "-":
+                print ("")
+                print(color.RED + "This position is already revealed or guessed. Choose a different one." + color.END)
+                print ("")
+                continue
+            return row, col
+        except ValueError:
+            print ("")
+            print(color.RED + "Invalid input. Please enter integers for row and column." + color.END)
+            print ("")
+            
 def all_pairs_matched():
     for row in player:
         if "-" in row:
             return False
     return True
-
-def get_valid_input():
-    while True:
-        row = int(input("Input the " + color.YELLOW + color.BOLD + color.UNDERLINE + "row" + color.END +": ")) - 1
-        col = int(input("Input the " + color.GREEN + color.BOLD + color.UNDERLINE +  "column" + color.END + ": ")) - 1
-        print ("")
-        if player[col][row] == "-":
-            return row, col
-        else:
-            print(color.RED + "This position is already revealed. Choose a different one." + color.END)
-            print ("")
-
-
+    
 board = [["Cat","Seal","Dog","Pig","Elephant"],
          ["Fly","Elephant","Fly","Cat","Frog"],
          ["Rabbit","Giraffe","Swan","Turkey","Frog"],
@@ -64,31 +74,32 @@ pairs = 12
 
 finish = False
 
-while not finish:
+while finish != True:
     print ("") 
     print (color.PURPLE + "You have " + color.BOLD +  str(pairs) + " pairs remaining to guess." + color.END)
     pairs = int(pairs)
     print ("")
-    print("Select the first word:")
-    first_r, first_c = get_valid_input()
-    player[first_c][first_r] = board[first_c][first_r]
+    first_r, first_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the first word: "), ("Input the" + color.CYAN + " column " + color.END +  "of the first word: "))
+    player[first_r][first_c] = board[first_r][first_c]
     progress()
 
-    print("Select the second word:")
-    second_r, second_c = get_valid_input()
-    player[second_c][second_r] = board[second_c][second_r]
+    print ("")
+    second_r, second_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the second word: "), ("Input the" + color.CYAN + " column " + color.END +  "of the second word: "))
+    player[second_r][second_c] = board[second_r][second_c]
     progress()
 
-    if board[first_c][first_r] != board[second_c][second_r]:
+    if board[first_r][first_c] != board[second_r][second_c]:
         print(color.RED + "Not a match" + color.END)
-        print ("")
-        player[first_c][first_r] = "-"
-        player[second_c][second_r] = "-"
+        player[first_r][first_c] = "-"
+        player[second_r][second_c] = "-"
+        
+    elif board[first_r][first_c] == "Giraffe":
+        player[first_r][first_c] = "Giraffe"
+        print ("This is the trick word!")
     else:
         pairs = pairs - 1
         print(color.GREEN + "It's a match!" + color.END)
-        print ("")
-
+        
     if all_pairs_matched():
         print(color.BOLD + color.GREEN + "Congratulations! You matched all pairs!" + color.END)
         finish = True
