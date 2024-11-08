b0e1da452a4439dd94daee861a3976f18e2995c6
Ibrahim M <145262661+Ibi-M@users.noreply.github.com>
2024-11-08 11:18:09 +0000

    Complete

--- bingo.py
+++ bingo.py
@@ -27,8 +27,6 @@ print ("You have 9 guesses in total!")
 print ("- - - - - - - - - - - - - - - - - - - - - - - - - - " + color.END)
 print ("")
 
-
-
 ans = []
 
 while len(ans) < 9:
@@ -40,7 +38,6 @@ solutions = [[ans[0], ans[1], ans[2]],
              [ans[3], ans[4], ans[5]],
              [ans[6], ans[7], ans[8]]]
 
-
 pos1 = "*"
 pos2 = "*"
 pos3 = "*"
@@ -50,7 +47,6 @@ pos6 = "*"
 pos7 = "*"
 pos8 = "*"
 pos9 = "*"
-
 
 ticket = [[pos1, pos2, pos3],
           [pos4, pos5, pos6],
@@ -63,19 +59,32 @@ colours = [[color.DARKCYAN, color.BLUE, color.YELLOW],
 
 def print_ticket(ticket):
     column_width = 15
-    
+
     print ((colours[0][0] + ticket[0][0] + color.END).center(column_width) + "|" + (colours[0][1] + ticket[0][1] + color.END).center(column_width) + "|" + (colours[0][2] + ticket[0][2] + color.END).center(column_width) )
     print ((colours[1][0] + ticket[1][0] + color.END).center(column_width) + "|" + (colours[1][1] + ticket[1][1] + color.END).center(column_width) + "|" + (colours[1][2] + ticket[1][2] + color.END).center(column_width) )
     print ((colours[2][0] + ticket[2][0] + color.END).center(column_width) + "|" + (colours[2][1] + ticket[2][1] + color.END).center(column_width) + "|" + (colours[2][2] + ticket[2][2] + color.END).center(column_width) )
 
-        
+
 finish = False
 guesses = 9
 
 while finish == False and guesses != 0:
     guesses = guesses - 1
-    guess = int(input("Guess a number between 1 and 20: "))
     
+    while True:
+        try:
+            guess = int(input("Guess a number between 1 and 20: "))
+            if 1 <= guess <= 20:
+                break
+            else:
+                print ("")
+                print(color.RED + "Please enter a number between 1 and 20." + color.END)
+                print ("")
+        except ValueError:
+            print ("")
+            print(color.RED + "Invalid input. Please enter a number between 1 and 20." + color.END)
+            print ("")
+
     if guess in ans:
         print ("")
         print(color.GREEN + "Correct!" + color.END)
@@ -89,7 +98,6 @@ while finish == False and guesses != 0:
         ticket[row][col] = guess
 
         print_ticket(ticket)
-
 
         if ticket[0][0] == str(solutions[0][0]) and ticket[0][1] == str(solutions[0][1]) and ticket[0][2] == str(solutions[0][2]):
             finish = True
@@ -132,5 +140,3 @@ if guesses == 0:
     print ("The numbers were:")
     print ("")
     print (solutions)
-
-       
--- memory_match.py
+++ memory_match.py
@@ -14,92 +14,100 @@ class color:
 
 def progress():
     column_width = 10
-    print ("____________________________________________________________________")
+    print("____________________________________________________________________")
     print(color.BOLD + color.UNDERLINE + "|" + " ".center(column_width) + "|" + color.CYAN + "1".center(column_width) + "|" + "2".center(column_width) + "|" + "3".center(column_width) + "|" + "4".center(column_width) + "|" + "5".center(column_width) + color.END)
     print(color.BOLD + color.YELLOW + "|" + "1".center(column_width) + "|" + color.END + player[0][0].center(column_width) + "|" + player[0][1].center(column_width) + "|" + player[0][2].center(column_width) + "|" + player[0][3].center(column_width) + "|" + player[0][4].center(column_width) + "|")
     print(color.BOLD + color.YELLOW + "|" + "2".center(column_width) + "|" + color.END + player[1][0].center(column_width) + "|" + player[1][1].center(column_width) + "|" + player[1][2].center(column_width) + "|" + player[1][3].center(column_width) + "|" + player[1][4].center(column_width) + "|")
     print(color.BOLD + color.YELLOW + "|" + "3".center(column_width) + "|" + color.END + player[2][0].center(column_width) + "|" + player[2][1].center(column_width) + "|" + player[2][2].center(column_width) + "|" + player[2][3].center(column_width) + "|" + player[2][4].center(column_width) + "|")
     print(color.BOLD + color.YELLOW + "|" + "4".center(column_width) + "|" + color.END + player[3][0].center(column_width) + "|" + player[3][1].center(column_width) + "|" + player[3][2].center(column_width) + "|" + player[3][3].center(column_width) + "|" + player[3][4].center(column_width) + "|")
     print(color.BOLD + color.YELLOW + "|" + "5".center(column_width) + "|" + color.END + player[4][0].center(column_width) + "|" + player[4][1].center(column_width) + "|" + player[4][2].center(column_width) + "|" + player[4][3].center(column_width) + "|" + player[4][4].center(column_width) + "|")
-    print ("-----------------------------------------------------------------")
+    print("-----------------------------------------------------------------")
+
 def get_valid_input(prompt_row, prompt_col):
     while True:
         try:
             row = int(input(prompt_row)) - 1
             col = int(input(prompt_col)) - 1
             if row < 0 or row >= 5 or col < 0 or col >= 5:
-                print ("")
+                print("")
                 print(color.RED + "Invalid position. Please choose a row and column between 1 and 5." + color.END)
-                print ("")
+                print("")
                 continue
             if player[row][col] != "-":
-                print ("")
+                print("")
                 print(color.RED + "This position is already revealed or guessed. Choose a different one." + color.END)
-                print ("")
+                print("")
                 continue
             return row, col
         except ValueError:
-            print ("")
+            print("")
             print(color.RED + "Invalid input. Please enter integers for row and column." + color.END)
-            print ("")
-            
+            print("")
+
 def all_pairs_matched():
     for row in player:
         if "-" in row:
             return False
     return True
-    
-board = [["Cat","Seal","Dog","Pig","Elephant"],
-         ["Fly","Elephant","Fly","Cat","Frog"],
-         ["Rabbit","Giraffe","Swan","Turkey","Frog"],
-         ["Dog","Pig","Bird","Bird","Swan"],
-         ["Seal","Turkey","Toad","Toad","Rabbit"]]
 
-player = [["-","-","-","-","-"],
-          ["-","-","-","-","-"],
-          ["-","-","-","-","-"],
-          ["-","-","-","-","-"],
-          ["-","-","-","-","-"]]
+board = [["Cat", "Seal", "Dog", "Pig", "Elephant"],
+         ["Fly", "Elephant", "Fly", "Cat", "Frog"],
+         ["Rabbit", "Giraffe", "Swan", "Turkey", "Frog"],
+         ["Dog", "Pig", "Bird", "Bird", "Swan"],
+         ["Seal", "Turkey", "Toad", "Toad", "Rabbit"]]
 
-print (color.BOLD + color.UNDERLINE + "You are going to play the Memory Game!" + color.END)
-print ("")
+player = [["-", "-", "-", "-", "-"],
+          ["-", "-", "-", "-", "-"],
+          ["-", "-", "-", "-", "-"],
+          ["-", "-", "-", "-", "-"],
+          ["-", "-", "-", "-", "-"]]
+
+print(color.BOLD + color.UNDERLINE + "You are going to play the Memory Game!" + color.END)
+print("")
 time.sleep(1)
-print (color.BOLD + color.UNDERLINE + "There are 12 pairs in the table; with one having no pair!")
-print ("")
+print(color.BOLD + color.UNDERLINE + "There are 12 pairs in the table; with one having no pair!")
+print("")
 time.sleep(1)
-print (color.BOLD + color.UNDERLINE + "Guess by inputting the row and the column of the chosen place that you would like to reveal the word!" + color.END)
-print ("")
+print(color.BOLD + color.UNDERLINE + "Guess by inputting the row and the column of the chosen place that you would like to reveal the word!" + color.END)
+print("")
 
 pairs = 12
-
 finish = False
 
 while finish != True:
-    print ("") 
-    print (color.PURPLE + "You have " + color.BOLD +  str(pairs) + " pairs remaining to guess." + color.END)
+    print("") 
+    print(color.PURPLE + "You have " + color.BOLD + str(pairs) + " pairs remaining to guess." + color.END)
     pairs = int(pairs)
-    print ("")
-    first_r, first_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the first word: "), ("Input the" + color.CYAN + " column " + color.END +  "of the first word: "))
+    print("")
+    first_r, first_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the first word: "), ("Input the" + color.CYAN + " column " + color.END + "of the first word: "))
     player[first_r][first_c] = board[first_r][first_c]
     progress()
 
-    print ("")
-    second_r, second_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the second word: "), ("Input the" + color.CYAN + " column " + color.END +  "of the second word: "))
+    print("")
+    second_r, second_c = get_valid_input(("Input the" + color.YELLOW + " row " + color.END + "of the second word: "), ("Input the" + color.CYAN + " column " + color.END + "of the second word: "))
     player[second_r][second_c] = board[second_r][second_c]
     progress()
 
     if board[first_r][first_c] != board[second_r][second_c]:
         print(color.RED + "Not a match" + color.END)
-        player[first_r][first_c] = "-"
-        player[second_r][second_c] = "-"
         
-    elif board[first_r][first_c] == "Giraffe":
-        player[first_r][first_c] = "Giraffe"
-        print ("This is the trick word!")
+        if board[first_r][first_c] == "Giraffe":
+            player[first_r][first_c] = "Giraffe"
+            print (color.YELLOW + color.BOLD + "You have revealed the extra word which has no pair!" + color.END)
+        else:
+            player[first_r][first_c] = "-"
+
+        if board[second_r][second_c] == "Giraffe":
+            player[second_r][second_c] = "Giraffe"
+            print (color.YELLOW + color.BOLD + "You have revealed the extra word which has no pair!" + color.END)
+        else:
+            player[second_r][second_c] = "-"
     else:
-        pairs = pairs - 1
+        pairs -= 1
         print(color.GREEN + "It's a match!" + color.END)
-        
+
     if all_pairs_matched():
+        print ("")
         print(color.BOLD + color.GREEN + "Congratulations! You matched all pairs!" + color.END)
+        print ("")
         finish = True
