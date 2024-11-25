0b78b4be49df9b758450293d4c1481233d58080d
Ibrahim M <145262661+Ibi-M@users.noreply.github.com>
2024-11-25 13:44:05 +0000

    Completed With Comments

--- 21.py
+++ 21.py
@@ -1,40 +1,36 @@
-class color: #Class - lists all colors
-    PURPLE = '\033[95m'
-    DARKCYAN = '\033[36m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
+from colorama import *
+init(autoreset=True)
+
+class color:
     UNDERLINE = '\033[4m'
-    END = '\033[0m'
+
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+
+print (f"{Fore.BLUE} Testing {Fore.RESET}")
 
 def get_player_turn(player): #Function - Gets each players input and checks if its valid
     while True:
         try:
-            count = int(input(f"{player}, how many numbers will you say (1, 2, or 3)? "))
+            count = int(input(f"\n{player}, how many numbers will you say (1, 2, or 3)? "))
             if 1 <= count <= 3: # Checks if the player input is in valid range
                 return count
             else:
-                print(color.RED + "Invalid choice! Please choose a number between 1 and 3." + color.END)
-                print ("")
+                print(f"{Fore.RED} Invalid choice! Please choose a number between 1 and 3. {Fore.RESET}")
         except ValueError:
-            print(color.RED + "Invalid input! Please enter a number." + color.END)
+            print(f"{Fore.RED}Invalid input! Please enter a number.{Fore.RESET}")
             print("")
 
-print (color.BOLD + color.UNDERLINE + "You are going to play 21!" + color.END)
-print ("")
+print (f"{color.UNDERLINE + Style.BRIGHT} You are going to play 21!")
 
 players = []
 for i in range(2): # Gathers the players' names
     while True:
-        name = input(f"Enter name for Player {i + 1}: ")
-        print ("")
+        name = input(f"\nEnter name for Player {i + 1}:")
         if name:
             players.append(name)
             break
         else:
-            print(color.RED + "Player name cannot be empty. Please enter a valid name." + color.END)
-            print ("")
+            print(f"{Fore.RED} Player name cannot be empty. Please enter a valid name.\n")
 
 current_number = 0
 player_index = 0
@@ -44,12 +40,10 @@ while current_number <= 21: # Loop until number reaches 21
     count = get_player_turn(player)
 
     current_number = current_number + count
-    print ("")
-    print(color.BOLD + color.DARKCYAN + player + color.END + color.BOLD + " chose to say " + color.GREEN + str(count) + color.END + color.BOLD + " numbers."+ color.END)
-    print (color.BOLD + color.YELLOW + "The current count is " + str(current_number), color.END)
-    print ("")
-        
+    print(f"{Style.BRIGHT + Fore.BLUE + player + Fore.RESET + Style.BRIGHT} chose to say {Fore.GREEN + str(count) + Style.BRIGHT} numbers.")
+    print(f"{Style.BRIGHT + Fore.YELLOW}The current count is {str(current_number)}")
+
     if current_number >= 21:
-        print(color.PURPLE + player + " says 21 and loses the game!" + color.END)
+        print(f"\n{Fore.GREEN + player} says 21 and loses the game!")
         
     player_index = (player_index + 1) % len(players)
--- bingo.py
+++ bingo.py
@@ -1,27 +1,22 @@
 import random
 import time
+from colorama import *
+init(autoreset=True)
 
-class color: # Class - Defines all colors used
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
+class color:
     UNDERLINE = '\033[4m'
-    END = '\033[0m'
+
+# Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 
 # Intro
-print (color.BOLD + "You are going to play Bingo!\n")
+print (f"{Style.BRIGHT}You are going to play Bingo!\n")
 time.sleep(1)
-print ("Get 3 in a row to win!\n")
+print (f"{Style.BRIGHT}Get 3 in a row to win!\n")
 time.sleep(1)
-print ("You can win: vertically, horizontally or diagonally!\n")
+print (f"{Style.BRIGHT}You can win: vertically, horizontally or diagonally!\n")
 time.sleep(1)
-print ("You have 9 guesses in total!")
-print ("- - - - - - - - - - - - - - - - - - - - - - - - - - \n" + color.END)
+print (f"{Style.BRIGHT}You have 9 guesses in total!")
+print (f"{Style.BRIGHT}- - - - - - - - - - - - - - - - - - - - - - - - - - \n")
 
 ans = []
 
@@ -48,21 +43,24 @@ ticket = [[pos1, pos2, pos3],
           [pos4, pos5, pos6], # The player's ticket
           [pos7, pos8, pos9]]
 
-colours = [[color.DARKCYAN, color.BLUE, color.YELLOW],
-           [color.PURPLE, color.GREEN, color.CYAN], # Colors the ticket to make readability easier
-           [color.YELLOW, color.DARKCYAN, color.PURPLE]]
+colours = [[Fore.CYAN, Fore.BLUE, Fore.GREEN],
+           [Fore.YELLOW, Fore.WHITE, Fore.YELLOW], # Colors the ticket to make readability easier
+           [Fore.GREEN, Fore.BLUE, Fore.CYAN]]
 
 
 def print_ticket(ticket): #Procedure - Prints the player ticket
     column_width = 15
 
-    print ((colours[0][0] + ticket[0][0] + color.END).center(column_width) + "|" + (colours[0][1] + ticket[0][1] + color.END).center(column_width) + "|" + (colours[0][2] + ticket[0][2] + color.END).center(column_width) )
-    print ((colours[1][0] + ticket[1][0] + color.END).center(column_width) + "|" + (colours[1][1] + ticket[1][1] + color.END).center(column_width) + "|" + (colours[1][2] + ticket[1][2] + color.END).center(column_width) )
-    print ((colours[2][0] + ticket[2][0] + color.END).center(column_width) + "|" + (colours[2][1] + ticket[2][1] + color.END).center(column_width) + "|" + (colours[2][2] + ticket[2][2] + color.END).center(column_width) )
+    print (f"{(colours[0][0] + ticket[0][0]).center(column_width)} | {(colours[0][1] + ticket[0][1]).center(column_width)} | {(colours[0][2] + ticket[0][2]).center(column_width)}")
+    print (f"{(colours[1][0] + ticket[1][0]).center(column_width)} | {(colours[1][1] + ticket[1][1]).center(column_width)} | {(colours[1][2] + ticket[1][2]).center(column_width)}")
+    print (f"{(colours[2][0] + ticket[2][0]).center(column_width)} | {(colours[2][1] + ticket[2][1]).center(column_width)} | {(colours[2][2] + ticket[2][2]).center(column_width)}")
 
 
 finish = False
 guesses = 9
+
+def output():
+     print (f"{Style.BRIGHT + Fore.GREEN + color.UNDERLINE}You have won!")  
 
 while finish == False and guesses != 0: # Game Loop
     guesses = guesses - 1
@@ -72,64 +70,62 @@ while finish == False and guesses != 0: # Game Loop
             if 1 <= guess <= 20:
                 break
             else:
-                print(color.RED + "\nPlease enter a number between 1 and 20.\n" + color.END)
+                print(f"\n{Fore.RED}Please enter a number between 1 and 20.\n")
         except ValueError:
-            print(color.RED + "\nInvalid input. Please enter a number between 1 and 20.\n" + color.END)
+            print(f"\n{Fore.RED} Invalid input. Please enter a number between 1 and 20.\n")
 
     if guess in ans:
-        print(color.GREEN + "\nCorrect!\n" + color.END)
+        print(f"\n{Fore.GREEN} Correct!\n")
         place = ans.index(guess)
-
         row = place // 3
         col = place % 3
-
         guess = str(guess)
         ticket[row][col] = guess
 
-        print_ticket(ticket)
+        print_ticket(ticket) 
 
         # ------------------ CHECKING OF HORIZONTAL ------------------
 
         if ticket[0][0] == str(solutions[0][0]) and ticket[0][1] == str(solutions[0][1]) and ticket[0][2] == str(solutions[0][2]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         elif ticket[1][0] == str(solutions[1][0]) and ticket[1][1] == str(solutions[1][1]) and ticket[1][2] == str(solutions[1][2]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         elif ticket[2][0] == str(solutions[2][0]) and ticket[2][1] == str(solutions[2][1]) and ticket[2][2] == str(solutions[2][2]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         # ------------------ CHECKING OF VERTICAL ------------------       
 
         elif ticket[0][0] == str(solutions[0][0]) and ticket[1][0] == str(solutions[1][0]) and ticket[2][0] == str(solutions[2][0]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         elif ticket[0][1] == str(solutions[0][1]) and ticket[1][1] == str(solutions[1][1]) and ticket[2][1] == str(solutions[2][1]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         elif ticket[0][2] == str(solutions[0][2]) and ticket[1][2] == str(solutions[1][2]) and ticket[2][2] == str(solutions[2][2]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         # ------------------ CHECKING OF DIAGONAL ------------------
 
         elif ticket[0][0] == str(solutions[0][0]) and ticket[1][1] == str(solutions[1][1]) and ticket[2][2] == str(solutions[2][2]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
         elif ticket[0][2] == str(solutions[0][2]) and ticket[1][1] == str(solutions[1][1]) and ticket[2][0] == str(solutions[2][0]):
             finish = True
-            print (color.BOLD + color.UNDERLINE + "You have won!" + color.END)
+            output()
 
     else: # If no matches have been found
-        print (color.RED + color.BOLD + "Incorrect!\n" + color.END)
+        print (f"{Fore.RED + Style.BRIGHT} Incorrect!\n")
 
 if guesses == 0: # If player has run out of guesses
-    print (color.RED + color.BOLD + color.UNDERLINE + "You have lost!" + color.END)
+    print (Fore.RED + Style.BRIGHT + color.UNDERLINE + "You have lost!")
     print ("\nThe numbers were:\n")
     print (solutions)
